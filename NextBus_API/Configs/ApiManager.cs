d2fcb4f579972965c5c07dc5e647adfbb25768a2
Binusha Samod <110772940+Samod99@users.noreply.github.com>
2023-03-08 14:04:57 +0530

    Initial commit

---
+++ NextBus_API/Configs/ApiManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Data.SqlClient;
+
+namespace NextBus_API.Configs
+{
+    public sealed class ApiManager
+    {
+        private readonly string Environment = Configs.Environment.Localhost.ToString();
+
+        private ApiManager() { }
+        private static ApiManager instance = null;
+        public static ApiManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ApiManager();
+                }
+                return instance;
+            }
+        }
+
+        #region GetConnectionString
+        public SqlConnectionStringBuilder GetConnectionString()
+        {
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            if (this.Environment == Configs.Environment.Localhost.ToString())
+            {
+                // Localhost
+                builder.DataSource = @".\SQLExpress";
+                builder.IntegratedSecurity = true;
+                builder.InitialCatalog = "NextBus_DB";
+            }
+            else if (this.Environment == Configs.Environment.Development.ToString())
+            {
+                // not created
+            }
+
+            return builder;
+        }
+        #endregion
+
+        #region GetEnvironment
+        public string GetEnvironment()
+        {
+
+            return this.Environment;
+        }
+        #endregion
+    }
+
+
+    enum APIResponse
+    {
+        Success,
+        Fail
+    }
+
+    enum Environment
+    {
+        Localhost,
+        Development
+    }
+}
