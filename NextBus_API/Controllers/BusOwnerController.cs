73fc4bfae86298da7e946d928d9628b0f52e3526
Binusha Samod <110772940+Samod99@users.noreply.github.com>
2023-03-09 20:37:40 +0530

    BusOwnerCRUD complete

---
+++ NextBus_API/Controllers/BusOwnerController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NextBus_API.Configs;
+using NextBus_API.Data;
+using NextBus_API.Models.DTO;
+using NextBus_API.Models.Entities;
+
+namespace NextBus_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BusOwnerController : ControllerBase
+    {
+        private readonly NextBusDbContext nextBusDbContext;
+
+        public BusOwnerController(NextBusDbContext nextBusDbContext)
+        {
+            this.nextBusDbContext = nextBusDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBusOwners()
+        {
+            var busOwners = await nextBusDbContext.BusOwners.ToListAsync();
+
+            return Ok(busOwners);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetBusOwnerById(Guid id)
+        {
+            var busOwner = await nextBusDbContext.BusOwners.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (busOwner != null)
+            {
+                return Ok(busOwner);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBusOwner(AddBusOwnerRequest addBusOwnerRequest)
+        {
+            // Convert DTO to Entity
+
+            var busOwner = new BusOwner()
+            {
+                Name = addBusOwnerRequest.Name,
+                NIC = addBusOwnerRequest.NIC,
+                Mobile1 = addBusOwnerRequest.Mobile1,
+                Mobile2 = addBusOwnerRequest.Mobile2,
+                Mobile3 = addBusOwnerRequest.Mobile3,
+                Email = addBusOwnerRequest.Email,
+                RegDate = addBusOwnerRequest.RegDate,
+                ServiceName = addBusOwnerRequest.ServiceName,
+                NoOfBuses = addBusOwnerRequest.NoOfBuses
+            };
+
+            busOwner.Id = Guid.NewGuid();
+            busOwner.BusOwnerCode = CodeMaster.GenerateCode("BOC");
+
+            await nextBusDbContext.BusOwners.AddAsync(busOwner);
+            await nextBusDbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBusOwnerById), new { id = busOwner.Id }, busOwner);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateBusOwner([FromRoute] Guid id, UpdateBusOwnerRequest updateBusOwnerRequest)
+        {
+            // Check if exists
+
+            var existsBusOwner = await nextBusDbContext.BusOwners.FindAsync(id);
+
+            if (existsBusOwner != null)
+            {
+                existsBusOwner.Name = updateBusOwnerRequest.Name;
+                existsBusOwner.NIC = updateBusOwnerRequest.NIC;
+                existsBusOwner.Mobile1 = updateBusOwnerRequest.Mobile1;
+                existsBusOwner.Mobile2 = updateBusOwnerRequest.Mobile2;
+                existsBusOwner.Mobile3 = updateBusOwnerRequest.Mobile3;
+                existsBusOwner.Email = updateBusOwnerRequest.Email;
+                existsBusOwner.RegDate = updateBusOwnerRequest.RegDate;
+                existsBusOwner.ServiceName = updateBusOwnerRequest.ServiceName;
+                existsBusOwner.NoOfBuses = updateBusOwnerRequest.NoOfBuses;
+
+                await nextBusDbContext.SaveChangesAsync();
+
+                return Ok(existsBusOwner);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteBusOwner(Guid id)
+        {
+            var existsBusOwner = await nextBusDbContext.BusOwners.FindAsync(id);
+
+            if (existsBusOwner != null)
+            {
+                nextBusDbContext.Remove(existsBusOwner);
+                await nextBusDbContext.SaveChangesAsync();
+
+                return Ok(existsBusOwner);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ NextBus_API/Migrations/20230309082848_BusOwners.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NextBus_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class BusOwners : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BusOwners",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BusOwnerCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NIC = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mobile3 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RegDate = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ServiceName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoOfBuses = table.Column<int>(type: "int", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BusOwners", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BusOwners");
+        }
+    }
+}
